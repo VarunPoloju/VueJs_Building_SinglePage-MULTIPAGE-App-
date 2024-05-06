98916c375542803a953861829605707a4f8a5bdf
varun <varunpoloju966@gmail.com>
2024-05-06 18:50:57 +0530

    redirecting and catching routes when invalid url is entered

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Page Not found! May be view Our<router-link to="/teams">Teams</router-link> </h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
-import TeamMembers from '@/components/teams/TeamMembers'
+import TeamMembers from '@/components/teams/TeamMembers';
+import NotFound from '@/components/nav/NotFound';
 
 import App from './App.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
-    {path: '/teams/:teamId', component: TeamMembers, props : true}
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
