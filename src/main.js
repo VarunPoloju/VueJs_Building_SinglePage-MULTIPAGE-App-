6b90c83ab77338b212f3131908eac8c909c0d54c
varun <varunpoloju966@gmail.com>
2024-05-07 13:28:20 +0530

    utilizing route metadata

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
+      meta: { needsAuth: true },
       components: {
         default: TeamsList,
         footer: TeamsFooter,
@@ -55,6 +56,12 @@ const router = createRouter({
 
 router.beforeEach(function (to, from, next) {
   console.log(to, from);
+  if (to.meta.needsAuth) {
+    console.log('Needs Auth');
+    next();
+  } else {
+    next();
+  }
   next();
 });
 
